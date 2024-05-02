b44f7bc1097234928ebe06e0e00533c8cad7efe8
PROGRAMMER <motafaka@yandex.ru>
2024-05-02 12:47:26 +0300

    CodeIsReady

--- src/test/java/ru/netology/banklogin/data/SQLHelper.java
+++ src/test/java/ru/netology/banklogin/data/SQLHelper.java
@@ -30,7 +30,7 @@ public class SQLHelper {
     public static void cleanDatabase() {
         var connection = getConn();
         QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
-        QUERY_RUNNER.execute(connection, "DELETE FROM card_translation");
+        QUERY_RUNNER.execute(connection, "DELETE FROM card_transactions");
         QUERY_RUNNER.execute(connection, "DELETE FROM cards");
         QUERY_RUNNER.execute(connection, "DELETE FROM users");
     }
